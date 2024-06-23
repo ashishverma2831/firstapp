2fed1329ddf3cc22fcc82095fd708c51f5972661
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-23 15:40:10 +0530

    chore: Update App.jsx to include HomeScreen component

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View style={{padding:50}}>
+        <Text style={{textAlign:'center',
+            fontSize:30,
+            fontWeight:'bold',
+        }}> My First React Native Hybrid App </Text>
+        <View style={{marginTop:20}}>
+            <Button title='Ready to Rock' />
+        </View>
+    </View>
+  )
+}
+
+export default HomeScreen
